6fd5ac2b8f4da8b4c59aed38926e483559fe2f21
SpecialNoel <jianminglin2893@gmail.com>
2026-07-05 00:13:01 -0400

    Fixed the issue existed in join-room feature

--- socket-chat/server/routes/rooms/join-room-handler.js
+++ socket-chat/server/routes/rooms/join-room-handler.js
@@ -15,33 +15,30 @@ async function handleJoinRoom(req, res) {
         // Fetch the role of this user in the room; assign it as a member if the membership is not existed yet
         const role = membership ? membership.role : "member";
 
-        // Send "join room" request to the server only if the user has never joined to the room before
-        if (!membership){
-            // Join the room
-            const joinRoomResult = await joinRoom(userObjectId, roomCode, role);
-            
-            // Handle join-room failure
-            if (!joinRoomResult.success) {
-                switch (joinRoomResult.reason) {
-                    case "ALREADY_IN_ROOM":
-                        return res.status(409).json({
-                            success: false,
-                            code: "ALREADY_IN_ROOM",
-                            error: "User already in room",
-                        });
-                    case "ROOM_NOT_FOUND":
-                        return res.status(404).json({
-                            success: false,
-                            code: "ROOM_NOT_FOUND",
-                            error: "Room not found",
-                        });
-                    default: 
-                        return res.status(500).json({
-                            success: false,
-                            code: "OTHER",
-                            error: "Join room failure",
-                        });
-                }
+        // Send "join room" request to the server
+        const joinRoomResult = await joinRoom(userObjectId, roomCode, role);
+        
+        // Handle join-room failure
+        if (!joinRoomResult.success) {
+            switch (joinRoomResult.reason) {
+                case "ALREADY_IN_ROOM":
+                    return res.status(409).json({
+                        success: false,
+                        code: "ALREADY_IN_ROOM",
+                        error: "User already in room",
+                    });
+                case "ROOM_NOT_FOUND":
+                    return res.status(404).json({
+                        success: false,
+                        code: "ROOM_NOT_FOUND",
+                        error: "Room not found",
+                    });
+                default: 
+                    return res.status(500).json({
+                        success: false,
+                        code: "OTHER",
+                        error: "Join room failure",
+                    });
             }
         }
 
--- socket-chat/server/socket/handlers/room-handler.js
+++ socket-chat/server/socket/handlers/room-handler.js
@@ -2,7 +2,6 @@
 
 import { addUserToRoom } from "../../services/redis-services/user-services.js";
 import { getMembersInRoom } from "../../services/db-services/membership/get-members-service.js";
-import { enterRoom } from "../../services/db-services/membership/enter-room-service.js";
 import { getMessages } from "../../services/db-services/message/get-messages-service.js";
 import { getRoomInfoForDisplay } from "../../services/db-services/room/get-room-info-for-display-service.js";
 
@@ -30,9 +29,6 @@ async function registerEnterRoomHandler(socket, roomCode) {
     // Enter the user to the room
     socket.currentRoomCode = roomCode;
     socket.join(roomCode);
-
-    // Re-active the membership of the user in the room
-    await enterRoom(socket.user.userObjectId, roomCode);
 
     // Fetch online users and message history of the room
     const onlineUsers = await getMembersInRoom(roomCode);
