049dcb0fb8730a4ebab895d767a6c1b9e612be8a
SpecialNoel <jianminglin2893@gmail.com>
2026-07-05 00:38:58 -0400

    Improved the room-deletion logic to set the active status of all users in the room to false when the room is deleted by the creator.

--- socket-chat/server/services/db-services/room/delete-room-service.js
+++ socket-chat/server/services/db-services/room/delete-room-service.js
@@ -1,16 +1,17 @@
 // delete-room-service.js
 
-import { Room } from "../../../models/room-model.js"
+import { Room } from "../../../models/room-model.js";
+import { Membership } from "../../../models/membership-model.js";
 
 // Delete the given room from the database
 async function deleteRoom(roomCode) {
     try {
         // await Room.deleteOne({ roomCode }); // Hard-delete
 
-        // Soft-delete: ”deleted” marked as true, messages 
+        // Soft-delete: ”deleted” marked as true, but messages 
         // still exist, and the room becomes inaccessible to everyone
         const date = new Date();
-        await Room.findOneAndUpdate(
+        const room = await Room.findOneAndUpdate(
             { 
                 roomCode: roomCode, 
                 deleted: false 
@@ -18,6 +19,26 @@ async function deleteRoom(roomCode) {
             {
                 deleted: true,
                 deletedAt: date
+            },
+            {
+                new: true // return the updated room document
+            }
+        );
+        // Handle room does not exist or already been deleted error
+        if (!room) {
+            return null;
+        }
+
+        // Set the "active" status of  all users in the room to false
+        await Membership.updateMany(
+            {
+                roomObjectId: room._id, // find all membership documents that are active and associated with the room
+                active: true,
+            },
+            {
+                $set: {
+                    active: false, // set the "active" statuses to false
+                },
             }
         );
         console.log("Room deleted from DB\n");
