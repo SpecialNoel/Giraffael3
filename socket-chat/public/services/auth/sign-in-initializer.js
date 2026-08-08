8d4ec13c4ae572b2c319a660ddc614a8bc45f1fe
SpecialNoel <jianminglin2893@gmail.com>
2026-08-07 23:48:16 -0400

    WIP: working on the username setting

--- socket-chat/public/services/auth/sign-in-initializer.js
+++ socket-chat/public/services/auth/sign-in-initializer.js
@@ -47,7 +47,7 @@ function signIn() {
 
             // Store necessary user information to the local storage of user's browser
             localStorage.setItem("userId", data.userId);     // userId (user public id)
-            localStorage.setItem("username", data.username); // username recorded in database
+            localStorage.setItem("username", data.username); // username recorded in database 
             localStorage.setItem("token", data.token);       // JWT token
 
             // The credentials are verified by server to be valid, proceed to the Dashboard page.
