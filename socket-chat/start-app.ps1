8358758f4b65fa9f310f44c61efb54d1290a31a0
SpecialNoel <jianminglin2893@gmail.com>
2026-09-01 22:41:46 -0400

    Added a file that starts the server on Windows machines

---
+++ socket-chat/start-app.ps1
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Start Redis in WSL if it isn't already running
+if ! wsl redis-cli ping >/dev/null 2>&1; then
+    echo "Starting Redis..."
+    wsl sudo service redis-server start
+else
+    echo "Redis already running"
+fi
+
+# Run the server of the application
+echo "Starting Node server..."
+node -r dotenv/config ./index.js
